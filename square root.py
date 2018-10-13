934a5c28efcb747c207a9dbc8f4c8405f53a477b
JN6D <JN6D@bigpond.com>
2018-10-13 12:03:02 +1000

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,12 @@
+from math import *
+print('Type in a number to find the square root of it!')
+x = input()
+x = float(x)
+
+if x < 0:
+    print("You can't sqaure root a negitive number.")
+    print("Open the file again after pressing enter.")
+    input()
+else:
+    print(sqrt(x))
+    input()
